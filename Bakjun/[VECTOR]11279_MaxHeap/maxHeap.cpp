8788c0ad209c76e77ac44159bc5a66197cf996f5
Youngwoo Park <36143152+parkyw1206@users.noreply.github.com>
2021-10-16 00:47:11 +0900

    10월 15일 백준 11279 최대힙

---
+++ Bakjun/[VECTOR]11279_MaxHeap/maxHeap.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include<stdio.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+using namespace std;
+
+int N;
+priority_queue<int> nums;
+
+int main()
+{
+    fastio;
+
+    cin >> N;
+    for(int i = 0 ; i < N ;i++){
+        int temp;
+        cin >> temp;
+        
+        if(temp == 0){
+            if(nums.empty()){
+                cout << "0\n";
+            }
+            else{
+                cout << nums.top() << "\n";
+                nums.pop();
+            }
+        }
+        else{
+            nums.push(temp);
+        }
+    }
+    
+    return 0;
+}
